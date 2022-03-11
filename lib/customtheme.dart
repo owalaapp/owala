bcf91b4b78c79859f458b1769e51559270101b65
Dev <devgargperso@gmail.com>
2022-03-11 10:22:03 +0530

    Changed font size

--- lib/customtheme.dart
+++ lib/customtheme.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 String primaryColor = "#25CF60";
-double h5FontSize = 24;
+double h5FontSize = 30;
 double regularFontSize = 16;
